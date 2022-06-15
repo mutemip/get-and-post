fe58ed21e8068d6e161ffef205d28aee502c3549
mutemip <paulmutemi2030@gmail.com>
2022-06-16 02:58:19 +0300

    POST and GET requests Practice

---
+++ index.js
@@ -0,0 +1,61 @@
+//event listener
+document.querySelector('#animal-form').addEventListener('submit', submitFn)
+
+//Event handlers
+function submitFn(e){
+    e.preventDefault()
+    let animalObj = {
+        anime_name:e.target.anime_name.value,
+        anime_img:e.target.anime_img
+    }
+    returnOneAnimal(animalObj)
+    postAnimal(animalObj)
+}
+
+
+// DOM render function
+function returnOneAnimal(animal){
+    //build animal
+    let tempView = document.createElement('li')
+    tempView.innerHTML = `
+        <div class="">
+            <h4>${animal.anime_name}</h4>
+        </div>
+        <img src="${animal.anime_img}">
+    `
+    //console.log(tempView);
+    //Add animal temView to DOM
+    document.querySelector('#animal-list').appendChild(tempView)
+}
+
+//Fetch requests
+//Fetch all animal respourse
+function fetchAllAnimalData(){
+    fetch('http://localhost:3000/aData')
+    //use results from the promise
+    .then(result => result.json())
+    //log translated data from json
+    .then(aData => aData.forEach(animal => returnOneAnimal(animal)))
+}
+
+
+function postAnimal(animalObj){
+    fetch('http://localhost:3000/aData', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(animalObj)
+    })
+    .then(result => result.json())
+    .then(animal => console.log(animal))
+}
+
+//initial Render
+//Get data and render it to the DOM
+function initialFn(){
+    fetchAllAnimalData()
+    //aData.forEach(animal => oneAnimal(animal));
+}
+initialFn()
+
